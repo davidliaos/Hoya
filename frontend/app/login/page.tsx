e10783e0a93e5638c6d162762ccc30712d08c2bc
prince-ao <prince.addo@cix.csi.cuny.edu>
2024-01-27 19:47:59 -0500

    everything

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -16,8 +16,10 @@ export default function LoginPage() {
 
   return (
     <div className="flex flex-col items-center justify-center h-[100vh] bg-gradient-to-br from-[#565f89] to-[#414868]">
-      <div className="flex flex-col bg-[#1a1b26] p-4 rounded-lg gap-4  w-[530px]">
-        <h1 className="text-white text-xl font-bold">Log In</h1>
+      <div className="flex flex-col bg-[#1a1b26] p-4 rounded-lg gap-4 w-[530px]">
+        <h1 className="text-white text-xl font-bold border-b-2 border-b-white pb-2">
+          Log In
+        </h1>
         <input
           className="w-29 h-8 ps-3 rounded-sm text-sm"
           type="text"
